a5af826d79e10acca6b7e0278354a42ecf282ad2
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2020-05-22 23:42:00 +0200

    Updated PingModule to display ping in ms

--- src/modules/PingModule/PingModule.ts
+++ src/modules/PingModule/PingModule.ts
@@ -3,7 +3,10 @@ import { canAnswer } from '../common';
 
 const handleMessage = (args: string[], message: d.Message, channels: string[] = []) => {
    if (canAnswer(message, channels)) {
-      message.reply('pong !');
+      message.reply('pong !').then((sentMessage) => {
+         const delay = sentMessage.createdAt.getTime() - message.createdAt.getTime();
+         sentMessage.edit(`${sentMessage.content} (${delay}ms)`);
+      });
    }
 };
 
