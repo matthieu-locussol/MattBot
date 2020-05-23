cf53c23bc3b6f0c780182b3cd15d16453a0c98db
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2020-05-23 18:11:50 +0200

    OsuModule now generates a speed & aim difficulty chart using chart.js

--- src/modules/OsuModule/OsuPP.ts
+++ src/modules/OsuModule/OsuPP.ts
@@ -1,53 +1,110 @@
+import * as fs from 'fs';
 var readline = require('readline');
-var osu = require('ojsama');
+import * as osu from 'ojsama';
+import { Chart } from 'chart.js';
+import { CanvasRenderService } from 'chartjs-node-canvas';
 
-var mods = osu.modbits.none;
-var acc_percent;
-var combo;
-var nmiss;
+// (aim + speed) / 2
+const CHART_WIDTH = 399;
+const CHART_HEIGHT = 40;
+const CHART_STRAINS = 70;
+const CHART_FILENAME = 'chart.png';
 
-// get mods, acc, combo, misses from command line arguments
-// format: +HDDT 95% 300x 1m
-var argv = process.argv;
+const parser = new osu.parser();
 
-for (var i = 2; i < argv.length; ++i) {
-   if (argv[i].startsWith('+')) {
-      mods = osu.modbits.from_string(argv[i].slice(1) || '');
-   } else if (argv[i].endsWith('%')) {
-      acc_percent = parseFloat(argv[i]);
-   } else if (argv[i].endsWith('x')) {
-      combo = parseInt(argv[i]);
-   } else if (argv[i].endsWith('m')) {
-      nmiss = parseInt(argv[i]);
+const average = (array: any[]) => array.reduce((p, c) => p + c, 0) / array.length;
+
+const parseArgs = (argv: string[]) => {
+   let args = {
+      mods: 0,
+      acc_percent: undefined,
+      combo: undefined,
+      nmiss: undefined,
+   };
+
+   for (let i = 2; i < argv.length; ++i) {
+      if (argv[i].startsWith('+')) {
+         args.mods = osu.modbits.from_string(argv[i].slice(1) || '');
+      } else if (argv[i].endsWith('%')) {
+         args.acc_percent = parseFloat(argv[i]);
+      } else if (argv[i].endsWith('x')) {
+         args.combo = parseInt(argv[i]);
+      } else if (argv[i].endsWith('m')) {
+         args.nmiss = parseInt(argv[i]);
+      }
    }
-}
 
-// Displays : <stars> <accuracy> <pp>
-var parser = new osu.parser();
+   return args;
+};
+
+const generateChart = (difficulty: osu.std_diff) => {
+   const data = difficulty.objects.map((o: osu.std_diff_hitobject) => o.strains);
+
+   const strains_max = CHART_STRAINS < data.length ? CHART_STRAINS : data.length;
+   const strains_length = data.length;
+   const step = strains_length / strains_max || 1;
+
+   const averages = [];
+   for (let i = 0; i < strains_max; ++i) {
+      // Slice of strains to sum
+      const slice = data.slice(i * step, (i + 1) * step);
+      // Average speed
+      const speed = average(slice.map((e) => e[0]));
+      // Average aim
+      const aim = average(slice.map((e) => e[1]));
+
+      averages.push({ speed, aim });
+   }
+
+   const configuration: Chart.ChartConfiguration = {
+      type: 'line',
+      data: {
+         labels: averages.map(() => ''),
+         datasets: [
+            {
+               data: averages.map((e) => e.speed),
+               borderWidth: 1,
+               borderColor: 'rgba(255, 107, 129, 1)',
+               backgroundColor: 'rgba(255, 107, 129, 0.3)',
+            },
+            {
+               data: averages.map((e) => e.aim),
+               borderWidth: 1,
+               borderColor: 'rgba(107, 129, 255, 1)',
+               backgroundColor: 'rgba(107, 129, 255, 0.3)',
+            },
+         ],
+      },
+   };
+
+   const canvas = new CanvasRenderService(CHART_WIDTH, CHART_HEIGHT, (chart: typeof Chart) => {
+      chart.defaults.scale.display = false;
+      chart.defaults.global.legend.display = false;
+      chart.defaults.global.elements.point.radius = 0;
+   });
+
+   fs.writeFileSync(CHART_FILENAME, canvas.renderToBufferSync(configuration));
+};
+
 readline
    .createInterface({
       input: process.stdin,
       terminal: false,
    })
    .on('line', parser.feed_line.bind(parser))
-   .on('close', function () {
-      var map = parser.map;
+   .on('close', () => {
+      const { mods, acc_percent, combo, nmiss } = parseArgs(process.argv);
 
-      var stars = new osu.diff().calc({ map: map, mods: mods });
+      const map = parser.map;
+      const stars = new osu.diff().calc({ map: map, mods: mods });
+      const pp = osu.ppv2({ stars, combo, nmiss, acc_percent });
 
-      var pp = osu.ppv2({
-         stars: stars,
-         combo: combo,
-         nmiss: nmiss,
-         acc_percent: acc_percent,
-      });
+      const starsResult = stars.toString().split(' ')[0];
+      // @ts-ignore
+      const accResult = pp.computed_accuracy.toString().split(' ')[0];
+      const ppResult = Math.round(pp.total * 100) / 100;
 
-      var max_combo = map.max_combo();
-      combo = combo || max_combo;
+      generateChart(stars);
 
-      process.stdout.write(
-         `${stars.toString().split(' ')[0]} ${pp.computed_accuracy.toString().split(' ')[0]} ${
-            pp.toString().split(' ')[0]
-         }`,
-      );
+      process.stdout.write(`${starsResult} ${accResult} ${ppResult}`);
    });
